5d2dbdb1d221aed0e2b8213b8854feae7fb6c2c2
Vycant <xehanort0730@hotmail.com>
2020-09-29 01:03:19 +0800

    - Implemented basics for Input Keys. To Do: Check previous commit.

---
+++ Assets/Scripts/Input/InputKeys.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+namespace RPG_Input
+{
+    public enum InputType
+	{
+        Accept,
+        Back,
+        Inspect,
+        None
+	}
+
+    public static class InputKeys
+    {
+        // Accept/Proceed Key
+        private static readonly KeyCode AcceptKey = KeyCode.Return;
+        private static readonly KeyCode AcceptKey2 = KeyCode.Space;
+        private static readonly KeyCode AcceptKey3 = KeyCode.F;
+
+        // Reject/Back Key
+        private static readonly KeyCode Back = KeyCode.Escape;
+
+        // Inspect Key
+        private static readonly KeyCode Inspect = KeyCode.E;
+
+        public static bool InputUpdate(out InputType _type)
+		{
+            if (GetAcceptKey())
+			{
+                _type = InputType.Accept;
+			}
+            else if (GetBackKey())
+			{
+                _type = InputType.Back;
+            }
+            else if (GetInspectKey())
+			{
+                _type = InputType.Inspect;
+            }
+            else
+			{
+                _type = InputType.None;
+                return false;
+			}
+
+            return true;
+        }
+
+        public static bool GetAcceptKey()
+		{
+            return Input.GetKeyDown(AcceptKey) || Input.GetKeyDown(AcceptKey2) || Input.GetKeyDown(AcceptKey3);
+		}
+
+        public static bool GetBackKey()
+		{
+            return Input.GetKeyDown(Back);
+		}
+
+        public static bool GetInspectKey()
+		{
+            return Input.GetKeyDown(Inspect);
+		}
+    }
+}
+
---
+++ Assets/Scripts/Input/InputManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace RPG_Input
+{
+    public class InputManager : MonoBehaviour
+    {
+		public static InputManager Instance { get; private set; }
+
+		private InputType _input;
+        // Start is called before the first frame update
+        void Awake()
+        {
+            if (Instance != null)
+			{
+                Destroy(gameObject);
+			}
+            else
+			{
+                Instance = this;
+			}
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (InputKeys.InputUpdate(out _input))
+			{
+                print("Input type: " + _input);
+			}
+        }
+    }
+}
+
+
